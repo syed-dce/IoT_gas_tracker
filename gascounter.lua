4070fcd6c9bd11bfbb2eef9f9c17ea6a86984dc6
syed-dce <syed7771@yahoo.com>
2024-05-16 20:43:04 +0530

    Fixed time sync. Fixed json format

--- gascounter.lua
+++ gascounter.lua
@@ -1,22 +1,9 @@
 print("GasCounter node started!")
 
---[[
-
-	Sleep cycle 0 begins at midnight. End of cycle 7 ends also at midnight.
-	Each cycle lasts 3 hours; at the end of each cycle, data from the AVR is 
-	dumped into the RTC memory.
-	At the end of cycle 7, which should occur around midnight, all RTC
-	memory is transferred to server and the sleep cycle is synchronized.
-
-	Sleep cycle information is stored in RTC memory slot 0.
-	Data logged by AVR is stored from slots 1-80 totalling 320 bytes per day.	
-
-]]--
-
 local cycle = rtcmem_get_sleep_cycle()
 
-if cycle == nil then
-	print("Sleep cycle uninitialized, getting it from server.")	
+if node.bootreason() == 0 or cycle == nil then
+	print("Syncronizing sleep plan with server...")	
 	do_api_call(false)	
 else	
 	print("Completed sleep cycle #" .. cycle)	
@@ -31,7 +18,7 @@ else
 		cycle = cycle + 1
 		rtcmem_set_sleep_cycle(cycle)	
 		print("Beginning sleep cycle #" .. cycle)
-		enter_sleep_cycle(cycle, conf.sleep.sleep_seconds, cycle == 7)
+		enter_sleep_cycle(cycle, conf.sleep.cycle_length, cycle == 7)
 	end
 end
 
