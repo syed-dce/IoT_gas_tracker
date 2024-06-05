7848eada60dd8d225e07857e599920a8d87e4aa3
syed-dce <syed7771@yahoo.com>
2024-06-05 21:17:13 +0530

    Aggregation by hour selected by default and other misc bug fixes

--- web/chartdata.php
+++ web/chartdata.php
@@ -7,15 +7,32 @@ if ($mysqli->connect_errno) {
     die("Error: " . $mysqli->connect_error . "\n");
 }
 
+switch ($_GET["agg"]) {
+    case "day":
+        $source = "pulses_over_day";
+        break;
+
+    case "hour":
+        $source = "pulses_over_hour";
+        break;
+
+    default:
+        $source = "pulses_over_time";
+        break;
+}
+
 ob_start("ob_gzhandler");
 header('Content-Type: text/plain');
 header('Cache-Control: no-cache');
 
-printf("ts,dm3\n");
+printf("ts,m3,vcc\n");
 
-if ($result = $mysqli->query("SELECT ts, (pulses * 10) as dm3 FROM pulses_over_time", MYSQLI_USE_RESULT)) {
+if ($result = $mysqli->query(
+    "SELECT ts, (pulses * 0.01) as m3, vcc FROM " . $source,
+    MYSQLI_USE_RESULT
+)) {
     while ($line = $result->fetch_assoc()) {
-        printf("%s,%s\n", $line['ts'], $line['dm3']);
+        printf("%s,%s,%.3f\n", $line["ts"], $line["m3"], $line["vcc"]);
     }
 }
 
